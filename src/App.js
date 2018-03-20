afc7af214b9081b3ac98a5732fd4587aefa435a9
Emobe <anthony.r.shuker@gmail.com>
2018-03-20 10:39:19 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './icons.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <i className="icon icon-nav-profile" style={{fontSize: '50px'}}/>
+      </div>
+    );
+  }
+}
+
+export default App;
